590cafc3215fc6ae65e39f1d77e03838f9e8de9b
yosoydead <radu-bogdan@outlook.com>
2018-09-06 16:31:05 +0300

    factorial and highest common factor are one level procedure

--- factorial/fact.py
+++ factorial/fact.py
@@ -2,6 +2,7 @@
 #ex: 5! = 5*4*3*2*1
 #0! = 1
 #it is for non-negative integers only
+#this is a one level procedure
 
 def fact(number):
     #to make one less calculation this could have been
--- highest common factor/hcf.py
+++ highest common factor/hcf.py
@@ -4,6 +4,7 @@
         divide p by q to give a remainder r
         if r == 0 then the hcf is q
         otherwise repeat with q and r taking place of p and q prime
+    this is a one level procedure
 '''
 
 #var 1
