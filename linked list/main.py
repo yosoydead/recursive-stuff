095e9915b3df29561189026cbaaf2c717d6965de
yosoydead <radu-bogdan@outlook.com>
2018-09-07 17:40:20 +0300

    getting started with a linked list

---
+++ linked list/main.py
@@ -0,0 +1,13 @@
+from node import Node
+
+def main():
+    # a = Node(1)
+    # b = Node(2)
+    print("a")
+    # a.nextNode = b
+    
+
+    # print(b.data)
+
+if __name__ == "__main__":
+    main()
